6e2f93d3f51410dbbe5a54f7b6c5044865e13cf1
Chaithra R Rao <chaithra.r@impelsys.com>
2025-02-04 10:52:53 +0530

    quiz and chart

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError, delay, retryWhen, scan, mergeMap, map } from 'rxjs/operators';
 
